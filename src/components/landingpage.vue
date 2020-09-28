7727298e1e790b318197841a2eb10e81b2e2953c
SvZ <king_7350@hotmail.com>
2020-09-28 10:24:47 -0400

    Updated UI design for Landing Page

--- src/components/landingpage.vue
+++ src/components/landingpage.vue
@@ -6,23 +6,23 @@
       <div
         class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
       >
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          Before they sold out
-          <br class="hidden lg:inline-block" />readymade
-          gluten
+        <h1
+          class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
+        >
+          Welcome to
+          <span class="hidden lg:inline-block text-blue-500">Uni Magazine</span>
         </h1>
         <p class="mb-8 leading-relaxed">
-          Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-          plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-          mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon
-          try-hard chambray.
+          Join today and become a member of the Stonewall University Magazine.
+          Our members have the ability to contribute in order to provide an
+          informative and insightful content for all.
         </p>
       </div>
-      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <div class="lg:max-w-2xl lg:w-full md:w-1/2 w-5/6">
         <img
           class="object-cover object-center rounded"
           alt="hero"
-          src="https://dummyimage.com/720x600"
+          src="https://s3-alpha-sig.figma.com/img/9b59/d4be/7c16628cc2711c4bd4267d74125b8cdf?Expires=1602460800&Signature=Sassofw1OecivZy3ICDUBXxjmqkmAKcwb7xTEZoCL9wxzub9yFGssMKNqgJl8XWGGZuWNKM4aA8X8yd6IUZezErWUfzaOGRqhlR~ddC8~LY3YKz0gvXEc-odK~EfSIGEOwRfl2gpRhOSYRK9ISkg57wbcjzq2Qz3ix5n-jIy3yLfDKoSF5ocYoN0zYy2QpN059VSUo8IoJMIJqCrMkrrRNULU8SIbGY5IyeKb7ZCyLV59E0nAgUbI140p~N0zMcC1OOtE9LSUU8G-34mcpoYeTt7SbXhh4g40z0ASSxnFDR2SmTIMWf2OQXLBq8h4DA8jES15szjD9ooyEBGP~gItg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
         />
       </div>
     </div>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,14 +1,37 @@
 <template>
   <header class="text-gray-900 body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <span class="ml-3 text-xl text-blue-600">Uni Magazine</span>
-      </a>
-      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center font-serif">
-        <router-link v-if="loggedIn" to="/home" class="mr-5 hover:text-gray-900 cursor-pointer">Home</router-link>
-        <router-link v-else to="/" class="mr-5 hover:text-gray-900 cursor-pointer">Home</router-link>
-        <a v-if="loggedIn" class="mr-5 hover:text-gray-900 cursor-pointer" @click="signOut">Sign Out</a>
-        <router-link v-else to="/login" class="mr-5 hover:text-gray-900 cursor-pointer">Login</router-link>
+    <div
+      class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+    >
+      <img src="../assets/images/logo.png" class="h-10" />
+
+      <nav
+        class="md:ml-auto flex flex-wrap items-center text-base justify-center font-serif"
+      >
+        <router-link
+          v-if="loggedIn"
+          to="/home"
+          class="mr-5 hover:text-gray-900 cursor-pointer"
+          >Home</router-link
+        >
+        <router-link
+          v-else
+          to="/"
+          class="mr-5 hover:text-gray-900 cursor-pointer"
+          >Home</router-link
+        >
+        <a
+          v-if="loggedIn"
+          class="mr-5 hover:text-gray-900 cursor-pointer"
+          @click="signOut"
+          >Sign Out</a
+        >
+        <router-link
+          v-else
+          to="/login"
+          class="mr-5 hover:text-gray-900 cursor-pointer"
+          >Login</router-link
+        >
       </nav>
     </div>
   </header>
@@ -32,7 +55,7 @@ export default {
     async signOut() {
       try {
         await firebase.auth().signOut();
-        this.$router.replace({ name: "Login" });
+        this.$router.replace({ name: "Home" });
       } catch (error) {
         console.log(error);
       }
